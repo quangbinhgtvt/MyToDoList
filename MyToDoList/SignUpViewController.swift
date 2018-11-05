7d2c73272e8fe9f55d70cd96823c090b60054f16
Quang Bình <quadamthephy.1988@gmail.com>
2018-11-05 23:25:18 +0700

    Update MyToDoList/SignUpViewController.swift

--- MyToDoList/SignUpViewController.swift
+++ MyToDoList/SignUpViewController.swift
@@ -42,22 +42,44 @@ class SignUpViewController: UIViewController {
     @IBAction func clickSignUpButton(_ sender: Any) {
         guard let email = emailTextField.text else {return}
         
-        if isValidEmail(email: email){
-            
-        }
-        else{
-            //alert here
-            Alert(title: "Invalid Email", message: "Enter a valid email (abc@xyz.com)")
-        }
+//        if isValidEmail(email: email){
+//            print("OK")
+//        }
+//        else{
+//            //alert here
+//            Alert(title: "Invalid Email", message: "Enter a valid email (abc@xyz.com)")
+//        }
         
         guard let pass = passwordTextField.text else {return}
         
         if isValidPass(pass: pass){
-            
+            print("OK")
         }
         else {
             Alert(title: "Weak Password", message: "Enter a password with at least 8 characters with at least 1 uppercase, 1 lowercase , 1 number and 1 special character")
         }
+        
+        //create user on firebase
+        
+        Auth.auth().createUser(withEmail: email, password: pass, completion: {(authResult, error) in
+            
+            if error != nil {
+            if let errCode = AuthErrorCode(rawValue: error!._code){
+                switch errCode {
+                case .invalidEmail:
+                    self.Alert(title: "Invalid Email", message: "Enter an valid email (eg. abc.111@gmail.com)")
+                case .emailAlreadyInUse:
+                    self.Alert(title: "Email Alreade in Use", message: "This email is already in use, try annother email or sign in")
+                default:
+                    self.Alert(title: "Fail", message: "Fail to Create a User")
+                }
+            }
+            }
+            
+            guard let user = authResult?.user else {return}
+
+            
+        })
         
     }
     
